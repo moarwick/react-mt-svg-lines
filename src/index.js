0e86745de1d2106404edaf658a0b34d5dad4c2cf
Ian Bateman <ian.h.bateman.18@dartmouth.edu>
2017-07-19 22:52:05 -0400

    switched to prop-types package to remove warning

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
 import { shortUID, clamp, trimFloat, isMsBrowser } from './utils.js'
 import TWEEN from 'tween.js'
